0c32aa9fc6e297bfce1ae8383d2e30f56298d276
abhiyanbhattarai <abhiyanbhattarai@gmail.com>
2024-02-08 17:51:15 -0500

    added user input field

--- app/src/main/java/com/example/abhiyanbhattarai_assign2/MainActivity.kt
+++ app/src/main/java/com/example/abhiyanbhattarai_assign2/MainActivity.kt
@@ -1,46 +1,94 @@
 package com.example.abhiyanbhattarai_assign2
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.annotation.SuppressLint
+import android.content.ContentResolver
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import android.content.ContentValues
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.abhiyanbhattarai_assign2.ui.theme.AbhiyanBhattarai_Assign2Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AbhiyanBhattarai_Assign2Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ContactsList(this)
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactsList(context: ComponentActivity) {
+    var contactName by remember { mutableStateOf("") }
+    var contactNumber by remember { mutableStateOf("") }
+// LaunchedEffect to perform data loading
+//    LaunchedEffect(Unit) {
+// Load contacts
+//        contacts = loadContacts(context)
+//    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = contactName,
+            onValueChange = { contactName = it },
+            label = { Text(text = "Contact Name") }
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = contactNumber,
+            onValueChange = { contactNumber = it },
+            label = { Text(text = "Contact Number") }
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+
+            }) {
+                Text("Load")
+            }
+            Button(onClick = {
+
+            }) {
+                Text("Add")
+            }
+        }
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContactManagerAppPreview() {
     AbhiyanBhattarai_Assign2Theme {
-        Greeting("Android")
+        ContactsList(ComponentActivity())
     }
 }
